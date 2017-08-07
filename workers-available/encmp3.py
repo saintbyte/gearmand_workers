ebdef37238708ba39106775931d395b9fabf5010
SB <admin@dotsb.net.ru>
2017-08-07 16:21:07 +0000

    before delete

--- workers-available/encmp3.py
+++ workers-available/encmp3.py
@@ -6,14 +6,18 @@ import os
 import signal
 import subprocess
 def wav2mp3(gearman_worker, gearman_job):
-    print gearman_jod.data+"\n\n"
+    print 'JOB:'+gearman_job.data+"\n\n"
     #gearman_worker.send_job_data(gearman_job, str(character))
     #gearman_worker.send_job_status(gearman_job, idx + 1, total_chars)
-    cmd = 'lame'
+    cmd = '/usr/bin/lame /var/www/upload.ural.im/wav/'+gearman_job.data+'.wav /var/www/upload.ural.im/mp3/'+gearman_job.data+'.mp3'
+    print cmd
     proc = subprocess.Popen(cmd, shell=True)
     proc.wait()
     return "OK"
 
+def simple(gearman_worker, gearman_job):
+    print 'JOB:'+gearman_jod.data+"\n\n"
+    return "OK"
 def handler_end(signum, frame):
     onend()
 
@@ -22,7 +26,9 @@ def main():
     signal.signal(signal.SIGQUIT, handler_end)
     gm_worker = gearman.GearmanWorker(['localhost:4730'])
     gm_worker.set_client_id('wav2mp3')
+    print 'started encmp3 main'+"\n";
     gm_worker.register_task('wav2mp3', wav2mp3)
+    gm_worker.register_task('simple', simple)
     try:
 	gm_worker.work()
     finally:
@@ -36,6 +42,7 @@ def onend():
 
 if __name__ == "__main__":
     pid = os.getpid()
-    fpid = open('pids/'+os.path.basename(__file__)+'.pid','w')
-    fpid.write(str(pid))
+    print 'started encmp3'+"\n";
+    #fpid = open('pids/'+os.path.basename(__file__)+'.pid','w')
+    #fpid.write(str(pid))
     main()
